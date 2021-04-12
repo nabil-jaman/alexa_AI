eca506dfeada5e231a0f6cb18c9c1e84f94dca2f
nabil-jaman <81367890+nabil-jaman@users.noreply.github.com>
2021-04-12 13:13:39 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            listener.adjust_for_ambient_noise(source)
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'hello' in command:
+        talk('hello sir, how may i help you?')
+    elif 'sex' in command:
+        talk('sir, sorry, i am not human')
+    elif 'are you single' in command:
+        talk('yes i am. i am in a relationship with my creator')
+    elif 'tell me about my sister' in command:
+        talk('sir, her name is, n a h i a. She is a 17 years old talented girl.')
+    else:
+        talk('sir, please, say the command again')
+
+
+while True:
+    run_alexa()
+
